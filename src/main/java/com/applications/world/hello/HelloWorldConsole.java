ae86ff80b425e6c16a1bfbe48cb09f618ccc8eca
Peter <peterknall@gmail.com>
2019-10-03 17:54:45 -0400

    Moved "Hello, World" text to method and added test case.

--- src/main/java/com/applications/world/hello/HelloWorldConsole.java
+++ src/main/java/com/applications/world/hello/HelloWorldConsole.java
@@ -2,7 +2,14 @@ package com.applications.world.hello;
 
 public class HelloWorldConsole {
 
+    public String printText() {
+        return "Hello, world!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        HelloWorldConsole hwc = new HelloWorldConsole();
+
+        System.out.println(hwc.printText());
     }
 }
---
+++ src/test/java/com/applications/world/hello/HelloWorldConsoleTest.java
@@ -0,0 +1,15 @@
+package com.applications.world.hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldConsoleTest {
+
+    @Test
+    public void printText() {
+        HelloWorldConsole hwc = new HelloWorldConsole();
+
+        assertEquals("Hello world!", hwc.printText());
+    }
+}
